88db514a529c7a07da396378ffe1c5578197d296
Esdras Rafael <esdrasrocha1122@gmail.com>
2018-06-19 09:56:59 -0300

    Criando classe Serie

---
+++ MySeriesList/ModelLibrary/Serie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary
+{
+    class Serie
+    {
+        public int ID_Serie { get; set; }
+        public string Nome { get; set; }
+        public int status { get; set; }
+        public int Tot_Ep { get; set; }
+        public int Ep_Atual { get; set; }
+        public string Genero1 { get; set; }
+        public string Genero2 { get; set; }
+        public string Genero3 { get; set; }
+        public int Nota { get; set; }
+        public string Observacao { get; set; }
+
+
+    }
+}
