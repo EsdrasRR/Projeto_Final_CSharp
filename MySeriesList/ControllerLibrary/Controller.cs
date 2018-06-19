2fffb9f96a8975b07f64431f9b75b065d0f9cd62
Esdras Rafael <esdrasrocha1122@gmail.com>
2018-06-19 10:03:02 -0300

    Criando Classe Controller e adicionando referencia

---
+++ MySeriesList/ControllerLibrary/Controller.cs
@@ -0,0 +1,59 @@
+﻿using ModelLibrary;
+using System.Collections.Generic;
+
+
+namespace ControllerLibrary
+{
+    public class Controller
+    {
+        // Lista
+        public List<Serie> ListaSeries { get; set; }
+
+        // Salvar
+        public void Salvar(Serie serie)
+        {
+            ListaSeries.Add(serie);
+        }
+
+        // Listar
+        public List<Serie> Listar()
+        {
+            return ListaSeries;
+        }
+
+        // Buscar por ID
+        public Serie BuscarPorID(int id)
+        {
+            foreach (Serie a in ListaSeries)
+            {
+                if (a.ID_Serie == id)
+                {
+                    return a;
+                }
+            }
+            return null;
+        }
+
+        // Editar 
+        public void Editar(int id, Serie serieAtualizada)
+        {
+            Serie serieAntiga = BuscarPorID(id);
+
+            if (serieAntiga != null)
+            {
+
+                serieAntiga.Nome = serieAtualizada.Nome;
+                serieAntiga.status = serieAtualizada.status;
+                serieAntiga.Nota = serieAtualizada.Nota;
+                serieAntiga.Ep_Atual = serieAtualizada.Ep_Atual;
+                serieAntiga.Tot_Ep = serieAtualizada.Tot_Ep;
+                serieAntiga.Observacao = serieAtualizada.Observacao;
+                serieAntiga.Genero1 = serieAtualizada.Genero1;
+                serieAntiga.Genero2 = serieAtualizada.Genero2;
+                serieAntiga.Genero3 = serieAtualizada.Genero3;
+
+            }
+        }
+
+    }
+}
--- MySeriesList/ModelLibrary/Serie.cs
+++ MySeriesList/ModelLibrary/Serie.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ModelLibrary
+﻿namespace ModelLibrary
 {
-    class Serie
+    public class Serie
     {
         public int ID_Serie { get; set; }
         public string Nome { get; set; }
