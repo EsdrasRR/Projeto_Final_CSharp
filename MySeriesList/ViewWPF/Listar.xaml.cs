92eda1cfe8f6b2f9db839fe9284f84dc2f9848a9
esdras rafael <esdrasrocha1122@gmail.com>
2018-06-27 20:35:56 -0300

    Alteraçoes na Listar xaml e xaml.cs

--- MySeriesList/ViewWPF/Listar.xaml.cs
+++ MySeriesList/ViewWPF/Listar.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ControllerLibrary;
+using ModelLibrary;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ViewWPF
 {
@@ -20,5 +12,30 @@ namespace ViewWPF
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            SerieController serieController = new SerieController();
+            dgSeries.ItemsSource = serieController.ListarTodos();
+
+        }
+
+        public void dgSeries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dg = ((DataGrid)sender);
+
+            Serie ser = (Serie)dg.Items[dg.SelectedIndex];
+
+            Editar editar = new Editar(ser);
+
+
+            editar.Show();
+
+            this.Close();
+
+
+
+        }
+    }
 }
