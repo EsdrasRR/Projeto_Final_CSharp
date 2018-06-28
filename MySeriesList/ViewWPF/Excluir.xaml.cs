06b9e2e57c8304ecb535e2b77d2773506a26b5d3
esdras rafael <esdrasrocha1122@gmail.com>
2018-06-27 22:05:24 -0300

    Alterando Excluir WPF

--- MySeriesList/ViewWPF/Excluir.xaml.cs
+++ MySeriesList/ViewWPF/Excluir.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ControllerLibrary;
+using ModelLibrary;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace ViewWPF
 {
@@ -19,9 +12,29 @@ namespace ViewWPF
 	/// </summary>
 	public partial class Excluir : Window
 	{
-		public Excluir()
+		public Excluir(Serie ser)
 		{
-			InitializeComponent();
+            _ser = ser;
+
+            InitializeComponent();
 		}
-	}
+        private Serie _ser;
+
+        public Excluir()
+        {
+        }
+
+        public void btnExcluir_Click(object sender, RoutedEventArgs e)
+        {
+            SerieController excluirSerie = new SerieController();
+
+            excluirSerie.Excluir(_ser.ID_Serie);
+
+
+            MessageBox.Show("Usuario excluido com exito!");
+
+            this.Close();
+
+        }
+    }
 }
---
+++ MySeriesList/ViewWPF/ListaExcluir.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System;
+using ControllerLibrary;
+using ModelLibrary;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace ViewWPF
+{
+    /// <summary>
+    /// Lógica interna para ListaExcluir.xaml
+    /// </summary>
+    public partial class ListaExcluir : Window
+    {
+        public ListaExcluir()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            SerieController serieController = new SerieController();
+            dgExSerie.ItemsSource = serieController.ListarTodos();
+
+        }
+
+        public void dgExSerie_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dg = ((DataGrid)sender);
+
+            Serie ser = (Serie)dg.Items[dg.SelectedIndex];
+
+            Excluir excluir = new Excluir(ser);
+
+
+            excluir.Show();
+
+            this.Close();
+
+
+
+        }
+    }
+}
